c4bda8bd990d5e2fdc138e909f11d618f882e2dc
Vad_Siam <nevod80@gmail.com>
2023-07-26 10:41:30 +0700

    looking bug

--- pages/api/openOrders.js
+++ pages/api/openOrders.js
@@ -1,8 +1,6 @@
 import { Spot } from '@binance/connector';
 
 export default async function handler(req, res) {
-  console.log(`Base URL1111`);
-  console.log(`Base URL: ${process.env.NEXT_BASE_URL}`);
   try {
     const apiKey = process.env.NEXT_PUBLIC_API_KEY;
     const apiSecret = process.env.NEXT_PUBLIC_API_SECRET;
@@ -12,23 +10,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json(response.data);
   } catch (error) {
-    console.error(error);
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error(error.response.data);
-      console.error(error.response.status);
-      console.error(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in Node.js
-      console.error(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error('Error', error.message);
-    }
-    console.error(error.config);
     return res.status(500).json({ error: `Error getting open orders: ${error}` });
   }
 }
